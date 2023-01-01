9e73259dbc27399564683ee8a5d7960067ab203f
Jolene Kearse <pharaohnutz@gmail.com>
2023-01-01 17:47:59 -0500

    feat/bug: add email/password sign up, unsuccessful

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+
 // modal references
 const overlay = document.getElementById("overlay");
 const signUpModal = document.getElementById("signUpModal");
 const logInModal = document.getElementById("logInModal");
 const logOutModal = document.getElementById("logOutModal");
+const firebaseConfig = {
+  apiKey: "AIzaSyB5OK46l_t2MIoXvh4n6jXnqAXNF0FxA2I",
+  authDomain: "book-tracker-a8777.firebaseapp.com",
+  projectId: "book-tracker-a8777",
+  storageBucket: "book-tracker-a8777.appspot.com",
+  messagingSenderId: "54050475125",
+  appId: "1:54050475125:web:ad511e971d50fd845d3c3c",
+};
+
+// initialize app
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
 
 // sign up modal controls
 document.getElementById("signUpModalBtn").addEventListener("click", () => {
@@ -30,4 +51,21 @@ document.getElementById("logOutModalBtn").addEventListener("click", () => {
 document.getElementById("closeLogOutModalBtn").addEventListener("click", () => {
   logOutModal.classList.add("hidden");
   overlay.style.backgroundColor = "transparent";
+});
+
+// sign up
+const signUpForm = document.querySelector("signUp");
+signUpForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // handle sign up with email and password
+  const email = signUpForm.email.value;
+  const password = signUpForm.password.value;
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+      console.log(`user created: ${cred.user}`);
+      signUpForm.reset();
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 });
